7418c7c263c16f3b79be4cb25dda8f78aedfcf20
superek <supereksuperek8@gmail.com>
2024-12-05 20:57:49 +0100

    Added template

---
+++ src/main/java/org/example/Day3/Day3.java
@@ -0,0 +1,12 @@
+package org.example.Day3;
+
+import org.example.Day1.Day1;
+
+import java.util.logging.Logger;
+
+public class Day3 {
+    private static final Logger logger = Logger.getLogger(Day1.class.getName());
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/org/example/template/Template.java
@@ -0,0 +1,12 @@
+package org.example.template;
+
+import org.example.Day1.Day1;
+
+import java.util.logging.Logger;
+
+public class Template {
+    private static final Logger logger = Logger.getLogger(Day1.class.getName());
+
+    public static void main(String[] args) {
+    }
+}
