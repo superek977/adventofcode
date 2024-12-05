e02a9f99072ce1dad56eeca81c186bd4643abf63
superek <supereksuperek8@gmail.com>
2024-12-05 16:46:08 +0100

    Init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.Day1.Day1;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
