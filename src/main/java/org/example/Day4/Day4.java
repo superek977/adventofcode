1999e1287446ce32218112cd3057d94c08e1f7a6
superek <supereksuperek8@gmail.com>
2024-12-07 10:27:28 +0100

    Skipping day 3 part 2 for now

---
+++ src/main/java/org/example/Day4/Day4.java
@@ -0,0 +1,13 @@
+package org.example.Day4;
+
+import org.example.Day1.Day1;
+
+import java.util.logging.Logger;
+
+public class Day4 {
+    private static final Logger logger = Logger.getLogger(Day4.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
